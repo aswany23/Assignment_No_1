7511b7d55173b033f1ec285d654551b19c698171
aswany23 <120076704+aswany23@users.noreply.github.com>
2023-01-31 13:20:09 +0530

    Create Program_no_04.py

---
+++ Program_no_04.py
@@ -0,0 +1,5 @@
+import random 
+X=('Anna ',' Meena ','kavya ')
+Y=('is going ','is jumbing ','is singing ')
+Z=('to school','to ground','to home')
+print(random.choice(X)+random.choice(Y)+random.choice(Z))
